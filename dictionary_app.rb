029a5f68b6c74e09dc8b040ac105543762e88299
Keelana Char <keelanachar@gmail.com>
2019-10-31 22:32:36 -0500

    prints all definitions and pronounciations

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -10,15 +10,22 @@ while true
     break
   end
 
-#   response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=YOURAPIKEY")
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=YOURAPI")
 
-#   definition = response.parse[0]["text"]
+  definitions = response.parse
+ 
+  definitions.each do |definition|
+    p definition["text"]
+  end
 
-#   p definition
-
-  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/topExample?useCanonical=false&api_key=YOURAPIKEY")
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/topExample?useCanonical=false&api_key=YOURAPI")
 
   top_example = response.parse["text"]
   p top_example
+
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/pronunciations?useCanonical=false&limit=50&api_key=YOURAPI")
+
+  pronounciation = response.parse[0]["raw"]
+  p pronounciation
 
 end
